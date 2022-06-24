52b52780fb659235525440172b465e30fb25ca2b
Miro Spönemann <miro.spoenemann@typefox.io>
2022-06-24 08:51:07 +0200

    Updated license to EPL-2.0

--- de.cau.cs.kieler.kiml.libavoid/src/de/cau/cs/kieler/kiml/libavoid/server/LibavoidServerPool.java
+++ de.cau.cs.kieler.kiml.libavoid/src/de/cau/cs/kieler/kiml/libavoid/server/LibavoidServerPool.java
@@ -1,16 +1,12 @@
-/*
- * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+/*******************************************************************************
+ * Copyright (c) 2013, 2022 Kiel University and others.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
  *
- * http://www.informatik.uni-kiel.de/rtsys/kieler/
- * 
- * Copyright 2013 by
- * + Kiel University
- *   + Department of Computer Science
- *     + Real-Time and Embedded Systems Group
- * 
- * This code is provided under the terms of the Eclipse Public License (EPL).
- * See the file epl-v10.html for the license text.
- */
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package de.cau.cs.kieler.kiml.libavoid.server;
 
 import java.util.LinkedList;
